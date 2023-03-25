5a66b77af594bef480876d2dcbfcfba9fb1ed79c
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2023-03-25 11:58:54 +0800

    update docker-build.hs

--- docker-build.sh
+++ docker-build.sh
@@ -7,25 +7,22 @@ docker scan bluet/tor-relay-proxy:latest
 
 docker tag bluet/tor-relay-proxy:latest bluet/tor-relay-proxy:v${VERSION}
 
-while true; do
+# ask for confirmation to push. looping unless y or n is entered
+while true ; do
         read -p "Add new git tag v${VERSION} and push? (Have you git add and git commit already?) [y/N]" yn
         case $yn in
                 [Yy]* ) git tag "v${VERSION}" -a -m "v${VERSION}" && git push && git push --tags; break;;
-                [Nn]* ) exit;;
-                * ) echo "";;
+                * ) break;;
         esac
 done
 
 # Fixes busybox trigger error https://github.com/tonistiigi/xx/issues/36#issuecomment-926876468
-docker run --privileged -it --rm tonistiigi/binfmt --install all
-
-docker buildx create --use
-
-while true; do
+while true ; do
         read -p "Build for multi-platform and push? (Have I Updated VERSION Info? Is the latest VERSION=${VERSION} ?) [y/N]" yn
         case $yn in
-                [Yy]* ) docker buildx build -t bluet/tor-relay-proxy:latest -t bluet/tor-relay-proxy:${VERSION} --platform linux/amd64,linux/arm64/v8,linux/386,linux/arm/v6,linux/arm/v7,linux/ppc64le,linux/s390x --pull --push .; break;;
-                [Nn]* ) exit;;
-                * ) echo "";;
+                [Yy]* ) docker run --privileged -it --rm tonistiigi/binfmt --install all \
+                        && docker buildx create --use \
+                        && time docker buildx build -t bluet/tor-relay-proxy:latest -t bluet/tor-relay-proxy:${VERSION} --platform linux/amd64,linux/arm64/v8,linux/386,linux/arm/v6,linux/arm/v7,linux/ppc64le,linux/s390x --pull --push .; break;;
+                * ) break;;
         esac
 done
